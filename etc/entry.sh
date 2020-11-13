9967cb7e47294d7d3259c9f3424bd2add6d9cf55
Florian M <esion@esion.net>
2020-11-13 01:44:11 +0100

    Bugs and typos

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+echo "Steam Login with user ${STEAM_USER}"
 bash "${STEAMCMDDIR}/steamcmd.sh" +login ${STEAM_USER} ${STEAM_PASS}\
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
-bash "${STEAMAPPDIR}/x64/server_linux32" \
+echo "Starting ${NS2_NAME} (${NS2_PLAYERLIMIT} slots)"
+bash "${STEAMAPPDIR}/x64/server_linux" \
 			-name "${NS2_NAME}" \
 			-map "${NS2_MAP}" \
-			-limit "${NS2_PLAYERLIMIT}"
-			-mods "${NS2_MODS}" \
+			-limit "${NS2_PLAYERLIMIT}" \
+			-mods "${NS2_MODS}"
 
