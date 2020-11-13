800cc9300ce0b1f9c9ee2df01634c455e4d14481
Florian M <esion@esion.net>
2020-11-13 23:20:56 +0100

    fixed bash errors

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l -c
-"id"
-"${STEAMCMDDIR}/steamcmd.sh" +login ${STEAM_USER} ${STEAM_PASS}\
+exec "id"
+exec "${STEAMCMDDIR}/steamcmd.sh" +login ${STEAM_USER} ${STEAM_PASS}\
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				validate \
 				+quit
 
-"${STEAMAPPDIR}/x64/server_linux" \
+exec "${STEAMAPPDIR}/x64/server_linux" \
 			-name "${NS2_NAME}" \
 			-map "${NS2_MAP}" \
 			-limit "${NS2_PLAYERLIMIT}" \
