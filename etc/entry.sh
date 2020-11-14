22800a12a1104e42229c9e36642d7c0e91b0e4d1
Florian M <esion@esion.net>
2020-11-14 05:22:22 +0100

    fixing things

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-bash "${STEAMCMDDIR}/steamcmd.sh" +login ${STEAM_USER} ${STEAM_PASS}\
+"${STEAMCMDDIR}/steamcmd.sh" +login ${STEAM_USER} ${STEAM_PASS}\
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				validate \
 				+quit
-bash -c "${STEAMAPPDIR}/x64/server_linux" \
-			-name "${NS2_NAME}" \
-			-map "${NS2_MAP}" \
-			-limit "${NS2_PLAYERLIMIT}" \
-			-mods "${NS2_MODS}"
+bash -c "${STEAMAPPDIR}/x64/server_linux \
+			-name \"${NS2_NAME}\" \
+			-map \"${NS2_MAP}\" \
+			-limit \"${NS2_PLAYERLIMIT}\" \
+			-mods \"${NS2_MODS}\""
 
