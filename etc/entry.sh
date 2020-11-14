907a6ff7c4b89895147e88a5892eb3371aa3ced5
Florian M <esion@esion.net>
2020-11-14 05:14:05 +0100

    fixing things

--- etc/entry.sh
+++ etc/entry.sh
@@ -4,7 +4,6 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login ${STEAM_USER} ${STEAM_PASS}\
 				+app_update "${STEAMAPPID}" \
 				validate \
 				+quit
-
 bash -c "${STEAMAPPDIR}/x64/server_linux" \
 			-name "${NS2_NAME}" \
 			-map "${NS2_MAP}" \
